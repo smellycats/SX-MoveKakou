cdc11a636df6222b72fc0d6049e05f912a384435
SmellyCat <smellycat2014@foxmail.com>
2016-01-20 20:14:49 +0800

    增加 date_upload字段

--- kakou/models/car.py
+++ kakou/models/car.py
@@ -7,17 +7,20 @@ class CarInfo(db.Model):
     """惠州黄标车数据"""
     __tablename__ = 'car_info'
     id = db.Column(db.Integer, primary_key=True)
+    date_upload = db.Column(db.DateTime, default=arrow.now().datetime)
     date_created = db.Column(db.DateTime, default=arrow.now().datetime)
     hphm = db.Column(db.String(16))
     wc = db.Column(db.String(255))
     img_path = db.Column(db.String(255))
     banned = db.Column(db.Integer, default=0)
 
-    def __init__(self, date_created=None, hphm='', wc='', img_path='', banned=0):
-        if date_created:
-            self.date_created = arrow.now().datetime
+    def __init__(self, date_upload=None, date_created=None, hphm='', wc='',
+                 img_path='', banned=0):
+        if date_upload:
+            self.date_upload = date_upload
         else:
-            self.date_created = date_created
+            self.date_upload = arrow.now().datetime
+        self.date_created = date_created
         self.hphm = hphm
         self.wc = wc
         self.img_path = img_path
